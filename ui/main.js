6d7896e4b8fd14587d2d49d7225af4d86d9127cf
saitejach1998 <saiteja.ch1998@gmail.com>
2018-03-18 17:51:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,23 @@
 console.log('Loaded!');
+
+
+var button = documet.getElementById("counter");
+
+button.onclick= function(){
+    
+    var request = new XMLHttpRequest();
+    request.readystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status == 200){
+                var counter = response.Text;
+                var span = documnet.getElementById("count");
+                span.innerHTML = counter.toString();
+            }   
+        }
+        
+    };
+    
+    request.open("GET","http://saitejach1998.imad.hasura-app.io/counter");
+    
+    request.send(null);
+    };
