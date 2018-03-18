be140b14ecb9aef81c73503b5d456d85c571000c
saitejach1998 <saiteja.ch1998@gmail.com>
2018-03-18 22:32:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,7 +17,7 @@ button.onclick= function(){
         
     };
     
-    request.open("GET","http://saitejach1998.imad.hasura-app.io/counter");
+    request.open("GET","http://saitejach1998.imad.hasura-app.io/counter",true);
     
     request.send(null);
     };
